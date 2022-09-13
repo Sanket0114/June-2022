4cdd9340880895008c037d848139604c6a2fefab
Sanket Tajane <Sanket Tajane@DESKTOP-5CDG864>
2022-09-13 20:06:19 +0530

    check

---
+++ Z_Project/src/Method/StaticFromDiffClass.java
@@ -0,0 +1,13 @@
+package Method;
+
+public class StaticFromDiffClass {
+	
+	public static void main(String[] args) {
+		StaticFromDiffClass1.m3();
+		StaticFromDiffClass1.m4();
+		StaticFromDiffClass1.m4();
+
+
+	}
+
+}
---
+++ Z_Project/src/Method/StaticFromDiffClass1.java
@@ -0,0 +1,10 @@
+package Method;
+
+public class StaticFromDiffClass1 {
+	public static void m3() {
+		System.out.println("Static Method From Diff Class m3");
+	}
+	public static void m4() {
+		System.out.println("Static Method From Diff Class m4");
+	}
+}
